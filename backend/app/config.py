62a8fb311f63c7ff0b880e9d187429b64f8705c4
M-Ibrahim2807 <mibrahimfirdousnaik@gmail.com>
2026-06-06 17:26:23 +0500

    backend cors /local host problem, (only receiving frontend request via localhost)

--- backend/app/config.py
+++ backend/app/config.py
@@ -17,7 +17,15 @@ class Settings(BaseSettings):
     database_url: str = "postgresql+psycopg2://postgres:postgres@localhost:5432/swapshift"
     admin_username: str = "admin"
     admin_password: str = "admin123"
-    cors_origins: str = "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174"
+    cors_origins: str = (
+    "http://localhost:3000,"
+    "http://127.0.0.1:3000,"
+    "http://localhost:5173,"
+    "http://127.0.0.1:5173,"
+    "http://localhost:5174,"
+    "http://127.0.0.1:5174,"
+    "https://swapshift-dev.vercel.app"
+)
     log_level: str = "INFO"
     request_cleanup_interval_seconds: int = 300
 
